44b20da1fe7063429a9918c751d42f0b244e40ca
BartekUciekalek <ewa7999@gmail.com>
2023-12-16 16:42:47 +0100

    Update layout, change value units to more web friendly ones, improve js code

--- js/script.js
+++ js/script.js
@@ -1,29 +1,20 @@
-logoSize = function () {
-  var theLogo = $("#container");
+$(document).ready(function () {
+  var $container = $("#container");
   var newImage = new Image();
-  newImage.src = theLogo.attr("src");
+  newImage.src = $container.attr("src");
   var imgWidth = newImage.width;
 
-  var maxScrollDistance = 468;
-
-  maxScrollDistance = Math.min(maxScrollDistance, $(window).height());
-
-  var widthAtMax = 468 * 1.333;
-
+  var maxScrollDistance = Math.min(468, $(window).height());
+  var widthAtMax = 624;
   var widthDiff = imgWidth - widthAtMax;
-
   var pixelsPerScroll = widthDiff / maxScrollDistance;
 
   $(window).scroll(function () {
     var scrollTopPos = Math.min($(document).scrollTop(), maxScrollDistance);
-
     var scrollChangePx = Math.floor(scrollTopPos * pixelsPerScroll);
-
-    // Adjust this line to make the image disappear
     var zoomedWidth = Math.max(widthAtMax + scrollChangePx, 0);
-
-    $(".container").css("background-size", zoomedWidth);
+    $container.css({
+      "background-size": zoomedWidth,
+    });
   });
-};
-
-logoSize();
+});
