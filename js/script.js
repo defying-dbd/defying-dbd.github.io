c84880f835b5544904e540ba37efc96b35e48149
BartekUciekalek <ewa7999@gmail.com>
2023-12-17 06:12:31 +0100

    Clean Up Code

--- js/script.js
+++ js/script.js
@@ -1,20 +1,20 @@
 $(document).ready(function () {
-  var $container = $("#container");
-  var newImage = new Image();
-  newImage.src = $container.attr("src");
-  var imgWidth = newImage.width;
+	var $container = $("#container");
+	var newImage = new Image();
+	newImage.src = $container.attr("src");
+	var imgWidth = newImage.width;
 
-  var maxScrollDistance = Math.min(468, $(window).height());
-  var widthAtMax = 624;
-  var widthDiff = imgWidth - widthAtMax;
-  var pixelsPerScroll = widthDiff / maxScrollDistance;
+	var maxScrollDistance = Math.min(468, $(window).height());
+	var widthAtMax = 624;
+	var widthDiff = imgWidth - widthAtMax;
+	var pixelsPerScroll = widthDiff / maxScrollDistance;
 
-  $(window).scroll(function () {
-    var scrollTopPos = Math.min($(document).scrollTop(), maxScrollDistance);
-    var scrollChangePx = Math.floor(scrollTopPos * pixelsPerScroll);
-    var zoomedWidth = Math.max(widthAtMax + scrollChangePx, 0);
-    $container.css({
-      "background-size": zoomedWidth,
-    });
-  });
+	$(window).scroll(function () {
+		var scrollTopPos = Math.min($(document).scrollTop(), maxScrollDistance);
+		var scrollChangePx = Math.floor(scrollTopPos * pixelsPerScroll);
+		var zoomedWidth = Math.max(widthAtMax + scrollChangePx, 0);
+		$container.css({
+			"background-size": zoomedWidth,
+		});
+	});
 });
