1826aa1af81a2e1ae60ff15f9d49ef3283ad5eab
im-h <138497029+im-h@users.noreply.github.com>
2023-12-20 23:10:28 +0100

    Added toggleable player information, Optimise and Clean Up Code

--- js/script.js
+++ js/script.js
@@ -1,5 +1,5 @@
 $(document).ready(function () {
-	function calculateBackgroundSize() {
+	function calculateLogoSize() {
 		return Math.max(
 			624 +
 				Math.floor(
@@ -10,21 +10,15 @@ $(document).ready(function () {
 		);
 	}
 
-	$("#container").css({
-		"background-size": calculateBackgroundSize() + "px",
-	});
+	function updateLogoSize() {
+		$("#container").css("background-size", calculateLogoSize() + "px");
+	}
 
-	$(window).scroll(function () {
-		$("#container").css({
-			"background-size": calculateBackgroundSize() + "px",
-		});
-	});
+	$(window).scroll(updateLogoSize);
 
 	$(".player").click(function () {
 		$(this).toggleClass("active");
-		if ($(this).hasClass("active")) {
-			$(".active").not($(this)).removeClass("active");
-		}
+		$(".active").not($(this)).removeClass("active");
 		$(this)
 			.closest(".section")
 			.find(".side-title, .section-title")
@@ -39,16 +33,9 @@ $(document).ready(function () {
 				.find(".side-title, .section-title")
 				.addClass("highlight");
 			$(this).siblings(".position-title").addClass("highlight");
-			$(".section, .position-title, .player")
-				.not($(this).closest(".section"))
-				.addClass("inactive");
-			$(this)
-				.removeClass("inactive")
-				.siblings(".position-title")
-				.removeClass("inactive");
 		},
 		function () {
-			$(".highlight, .inactive").removeClass("highlight inactive");
+			$(".highlight").removeClass("highlight");
 		}
 	);
 });
