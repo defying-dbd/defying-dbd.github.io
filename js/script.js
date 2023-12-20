451f7b313b6eea71848d3117c021c7645c0dbf7a
im-h <138497029+im-h@users.noreply.github.com>
2023-12-20 23:01:25 +0100

    Added toggleable player information, Optimise and Clean Up Code

--- js/script.js
+++ js/script.js
@@ -1,20 +1,35 @@
 $(document).ready(function () {
-	var $container = $("#container");
-	var newImage = new Image();
-	newImage.src = $container.attr("src");
-	var imgWidth = newImage.width;
-	var maxScrollDistance = Math.min(468, $(window).height());
-	var widthAtMax = 624;
-	var widthDiff = imgWidth - widthAtMax;
-	var pixelsPerScroll = widthDiff / maxScrollDistance;
+	function calculateBackgroundSize() {
+		return Math.max(
+			624 +
+				Math.floor(
+					$(document).scrollTop() *
+						(new Image().width - 624 / Math.min(468, $(window).height()))
+				),
+			0
+		);
+	}
+
+	$("#container").css({
+		"background-size": calculateBackgroundSize() + "px",
+	});
 
 	$(window).scroll(function () {
-		var scrollTopPos = Math.min($(document).scrollTop(), maxScrollDistance);
-		var scrollChangePx = Math.floor(scrollTopPos * pixelsPerScroll);
-		var zoomedWidth = Math.max(widthAtMax + scrollChangePx, 0);
-		$container.css({
-			"background-size": zoomedWidth,
+		$("#container").css({
+			"background-size": calculateBackgroundSize() + "px",
 		});
+	});
+
+	$(".player").click(function () {
+		$(this).toggleClass("active");
+		if ($(this).hasClass("active")) {
+			$(".active").not($(this)).removeClass("active");
+		}
+		$(this)
+			.closest(".section")
+			.find(".side-title, .section-title")
+			.toggleClass("active");
+		$(this).siblings(".position-title").toggleClass("active");
 	});
 
 	$(".player").hover(
@@ -24,9 +39,16 @@ $(document).ready(function () {
 				.find(".side-title, .section-title")
 				.addClass("highlight");
 			$(this).siblings(".position-title").addClass("highlight");
+			$(".section, .position-title, .player")
+				.not($(this).closest(".section"))
+				.addClass("inactive");
+			$(this)
+				.removeClass("inactive")
+				.siblings(".position-title")
+				.removeClass("inactive");
 		},
 		function () {
-			$(".highlight").removeClass("highlight");
+			$(".highlight, .inactive").removeClass("highlight inactive");
 		}
 	);
 });
